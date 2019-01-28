df43f315a5b27765c5724d55377a8036eb34eda3
nong0324 <nong0324@naver.com>
2019-01-28 23:30:19 +0900

    [추가] 하나의 문서 객체를 선택하는 메서드

---
+++ src/RohGeonHee/src/sample1-3/sample1-3.js
@@ -0,0 +1,12 @@
+// ID명으로 선택
+var title = document.getElementById('title');
+
+title.style.color = 'blue';
+title.style.background = 'orange';
+title.innerHTML = "JavaScript로 문서를 조작";
+
+// css선택자를 사용하는 방식
+var li = document.querySelector('li');
+
+li.style.color = "red";
+li.style.background = "blue";
