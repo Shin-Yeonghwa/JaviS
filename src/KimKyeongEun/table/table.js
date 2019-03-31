7a6f0b5dc82c89e9a4a1fd3e2b97820fe3ea502a
gke2476 <gke2476@gmail.com>
2019-03-31 23:23:47 +0900

    table 수정

--- src/KimKyeongEun/table/table.js
+++ src/KimKyeongEun/table/table.js
@@ -3,7 +3,6 @@ var createTable = function(){
         codeHtml : '',
         codeContent : document.querySelector('.table_code'),
         captionInput : document.getElementsByName('caption'),
-        thPosition : document.getElementsByName('thPosition'),
         captionText : document.getElementById('captionText'),
         colCount : document.getElementById('colCount'),
         colWidthInput : document.getElementsByName('colWidth'),
@@ -12,11 +11,15 @@ var createTable = function(){
         colBox : document.querySelector('.colbox'),
         btnCreate : document.getElementsByClassName('btn_create')[0],
         colunit : document.getElementsByName('colUnit'),
+        thPosition : document.getElementsByName('thPosition'),
         colunitCheck : '',
-        coldiffr : false
+        colWidthOpCheck : '',
+        colCurrentCheckPre : 0
     }
 
     //option check
+
+    //caption 상태값 체크
     function captionOpCheck(){
         var captionChecked =  null;
 
@@ -32,6 +35,7 @@ var createTable = function(){
         }else return captionChecked;
     }
 
+    //th 위치 값 체크
     function thOpCheck(){
         var  thChecked = null;
 
@@ -44,6 +48,7 @@ var createTable = function(){
         return thChecked;
     }
 
+    //td 행/열 개수 체크
     function tdCountOpCheck(){
         var rowCount = document.getElementById('rowCount'),
             colCount = document.getElementById('colCount');
@@ -62,6 +67,7 @@ var createTable = function(){
         }else return false;
     }
 
+    //col width (동등 or 다르게) 체크  + 다르게 일때 단위 선택 체크
     function colSetOpCheck(){
         var colSet =  null,
             colunitCheck =null;
@@ -85,7 +91,56 @@ var createTable = function(){
             }else return [colSet,colunitCheck];
         }else return [colSet];  //동등하게 나눌때 return
     }
+    
+    function colWidthChange(e){
+        v.colWidthOpCheck = e.id;
+    }
 
+    //col width담을 input 생성
+    function colWidthHandler(){
+        var colCurrentCheck = Number(document.getElementById('colCount').value);
+        
+        if(v.colWidthOpCheck === 'colWidthDif'){
+            if(colCurrentCheck !== 0 && (v.colBox.innerHTML == '')){
+
+                var colString ='<ol class="col_list">';
+    
+                for(var i = 0; i < colCurrentCheck ;i++){
+                    colString += '<li><input type="number" min="0" class="inp_col_width"></li>';
+                }
+    
+                colString += '</ol>'
+    
+                v.colBox.innerHTML = colString;
+            }else if(colCurrentCheck !== 0 && v.colBox.innerHTML !== ''){
+
+                var count = document.querySelector('.col_list').getElementsByTagName('li');
+
+                if(v.colCurrentCheckPre < colCurrentCheck){
+                    var remainder = colCurrentCheck -v.colCurrentCheckPre;
+                    for(var i = 0; i < remainder; i++){
+                        var liNode = document.createElement("li");
+                        liNode.innerHTML = '<input type="number" min="0" class="inp_col_width">';
+                        document.querySelector('.col_list').appendChild(liNode);
+                    }
+                }else if(v.colCurrentCheckPre > colCurrentCheck){
+                    var remainder = v.colCurrentCheckPre - colCurrentCheck;
+                    for(var i = 0; i < remainder; i++){
+                        document.querySelector('.col_list').removeChild(count[colCurrentCheck-1]);
+                    }
+                }
+
+            }else if(colCurrentCheck == 0){
+                alert("열 갯수 체크 좀");
+                document.getElementById('colCount').focus();
+            }
+            v.colCurrentCheckPre = colCurrentCheck;
+        }
+    }
+
+    
+    //html 추가 코드
+    //caption html 추가
     function captionContent(caption){
         captionText = v.captionText.value;
 
@@ -96,21 +151,16 @@ var createTable = function(){
             case 'captionHide' :
                 v.codeHtml += '<caption class="blind">';
             break;
-            case null : alert("caption 상태 체크해주세요.");
-                return false;
-            break;
         }
 
         return v.codeHtml += captionText + '</caption>';
     }
 
+    // colgorup html 추가
     function colWidthCheck(colSetCheck){
 
-        if(colSetCheck[0] == 'colWidthSame'){
-            document.querySelector('.unit').style.display = 'none';
-        }else{
-            document.querySelector('.unit').style.display = 'block';
-
+        if(colSetCheck[0] !== 'colWidthSame'){
+        
             var colUnitCheck = colSetCheck[1] == 'colPercent' ? '%' : 'px';
 
             if(v.colBox.innerHTML !== ''){
@@ -135,38 +185,21 @@ var createTable = function(){
         }
     }
 
-    function colWidthHandler(){
-        var colCurrentCheck = Number(document.getElementById('colCount').value);
-
-        if(colCurrentCheck !== 0){
-            var colString ='<ol class="col_list">';
-
-            for(var i = 0; i < colCurrentCheck ;i++){
-                colString += '<li><input type="number" min="0" class="inp_col_width"></li>';
-            }
-
-            colString += '</ol>'
-
-            v.colBox.innerHTML = colString;
-        }else{
-            alert("열 갯수 체크 좀");
-            document.getElementById('colCount').focus();
-        }
-    }
-
+    //최종 table 생성
     function createTableHandler(){
 
+        //상태값 체크
         var captionChecked = captionOpCheck();
         var thChecked = thOpCheck();
         var countCheck = tdCountOpCheck();
         var colSetCheck = colSetOpCheck();
 
+        // 모두 값이 있으면 table 생성
         if(captionChecked && thChecked && countCheck && colSetCheck){
 
             v.codeHtml = '<table>';
             captionContent(captionChecked);
             colWidthCheck(colSetCheck);
-            // v.codeHtml += this.colWidthCheck();
 
             rowCount = countCheck[0];
             colCount = countCheck[1];
@@ -178,9 +211,9 @@ var createTable = function(){
 
                     for(var j =0; j < colCount; j++){
 
-                        if(v.thChecked === 'thTop' && i === 0){
+                        if(thChecked === 'thTop' && i === 0){
                             v.codeHtml += '<th></th>'
-                        }else if(v.thChecked === 'thLeft' && j === 0){
+                        }else if(thChecked === 'thLeft' && j === 0){
                             v.codeHtml += '<th></th>'
                         }else{
                             v.codeHtml += '<td></td>';
@@ -196,10 +229,24 @@ var createTable = function(){
 
 
     return function(){
-        v.colWidthDif.addEventListener("click",function(){colWidthHandler()}.bind(this),false);
-        v.colWidthSame.addEventListener("click",function(){document.querySelector('.unit').style.display = 'none';});
+        v.colWidthDif.addEventListener("click",function(e){
+            document.querySelector('.unit').style.display = 'block';
+            document.querySelector('.colbox').style.display = 'block';
+            colWidthChange(e.target);
+            colWidthHandler();
+        }.bind(this),false);
+        v.colWidthSame.addEventListener("click",function(){
+            document.querySelector('.unit').style.display = 'none';
+            document.querySelector('.colbox').style.display = 'none';
+            colWidthChange(e);
+        });
+
+        //bind 사용
         v.btnCreate.addEventListener("click",function(){createTableHandler()}.bind(this),false);
-        v.colCount.addEventListener("change",function(){colWidthHandler()}.bind(this),false);
+        v.colCount.addEventListener("change",function(e){
+            console.log(e.target.value);
+            colWidthHandler()
+        }.bind(this),false);
     }
 }
 
