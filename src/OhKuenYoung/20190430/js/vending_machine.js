4562f92dec1732a2b551cf0a61c34a9b92f253f7
OhKuenYoung <rppjpp@in-comms.co.kr>
2019-06-11 17:48:56 +0900

    자판기 업데이트

--- src/OhKuenYoung/20190430/js/vending_machine.js
+++ src/OhKuenYoung/20190430/js/vending_machine.js
@@ -1,23 +1,22 @@
-
 var coin_array = new Array();
 
 document.getElementById('Input_amount').onsubmit = function() {
     var insert_coin = document.getElementById('money_inlet');
     var create_log = document.createElement('div');
     var log_text = insert_coin.value;
+    var coin_num = parseInt(insert_coin.value);
+    var coin_sum = (accumulator, currentValue) => accumulator + currentValue;
 
     create_log.className = "coin_log";
     create_log.innerHTML = "<span class='coin'>" + log_text + "</span>" + "원을 넣었습니다.";
 
     if(insert_coin.value <= 0) {
-        alert('0원 이상을 입력해 주세요.')  
+        alert('0원 이상을 입력해 주세요.')
     }
     else {
-        coin_array.push(insert_coin.value);
+        coin_array.push(coin_num);
         document.getElementById('Coin_log').appendChild(create_log);
     };
 
-    var coin_sum = document.getElementById('Current_amount');
-
-    coin_sum.innerHTML = coin_array.reduce((a, b) => a + b);
+    document.getElementById('Current_amount').innerText = coin_array.reduce(coin_sum);
 }
