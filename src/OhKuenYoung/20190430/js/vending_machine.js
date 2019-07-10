9a5b1bf02ad05adb962230653147b08a0e53e9f8
OhKuenYoung <rppjpp@in-comms.co.kr>
2019-07-10 16:48:47 +0900

    [190710] 자판기 업로드.

--- src/OhKuenYoung/20190430/js/vending_machine.js
+++ src/OhKuenYoung/20190430/js/vending_machine.js
@@ -1,6 +1,7 @@
 //동전 삽입 로그 + 현재금액
 var CoinSum = 0;
 
+//동전 투입
 function CashInsert(Price) {
     var Coin = Price.innerHTML.replace(/[^0-9]/g,"");
     var CreateLog = document.createElement('div');
@@ -11,12 +12,15 @@ function CashInsert(Price) {
     CreateLog.innerHTML = "<span class='coin'>" + Coin + "</span>" + "원을 넣었습니다.";
     document.getElementById('Coin_log').appendChild(CreateLog);
     document.getElementById('Current_amount').innerHTML = CoinSum;
+
+    DrinkCheck()
 }
 
+//음료수 선택
 function BuyDrink(Drink) {
     var DrinkPrice = Drink.innerHTML.replace(/[^0-9]/g,"");
-
     var CreateLog = document.createElement('div');
+    var DrinkKind = Drink.parentNode.getElementsByTagName('strong')[0];
 
     if(DrinkPrice > CoinSum) {
         alert('금액이 모자랍니다.');
@@ -25,32 +29,41 @@ function BuyDrink(Drink) {
         document.getElementById('Current_amount').innerHTML = CoinSum;
 
         CreateLog.className = "coin_log";
-        CreateLog.innerHTML = "<span class='coin'>" + DrinkPrice + "</span>" + "원을 사용하여 ";
+        CreateLog.innerHTML = "<span class='coin'>" + DrinkPrice + "원을 사용하여 " + DrinkKind.innerText + "를 뽑았습니다." + "</span>";
         document.getElementById('Coin_log').appendChild(CreateLog);
 
     };
+
+    DrinkCheck()
 };
 
+
+//반환
 function ReturnCash() {
 
     var CurrentAmount = CoinSum;
     CoinSum -= CurrentAmount;
 
     document.getElementById('Current_amount').innerHTML = 0;
+
+    var CreateLog = document.createElement('div');
+    CreateLog.innerHTML = "<span class='coin'>" + "현재 금액을 반환 하였습니다." + "</span>";
+    document.getElementById('Coin_log').appendChild(CreateLog);
+
+    DrinkCheck()
 }
 
-var MachineLog = {
+function DrinkCheck() {
+    var DrinkList = document.getElementsByClassName('drinkprice');
+    var Money = document.getElementById('Current_amount');
 
-    BuyDrink: function (Drink) {
-        DrinkPrice;
-    },
+    Money.innerHTML;
 
-    DrinkBuyLog: function() {
-        var CreateLog = document.createElement('div');
-        CreateLog.className = "coin_log";
-        CreateLog.innerHTML = "<span class='coin'>" + DrinkPrice + "</span>" + "원을 사용하였습니다";
-        document.getElementById('Coin_log').appendChild(CreateLog);
+    for(var i = 0 ; i < DrinkList.length ; i++){
+        if(DrinkList[i].innerHTML.replace(/[^0-9]/g,"") <= CoinSum ){
+            DrinkList[i].disabled = false;
+        } else {
+            DrinkList[i].disabled = true;
+        }
     }
-};
-
-
+}
