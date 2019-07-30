c3d8fbe2efc376772cf0f8e33a4e1f3bc73f1359
OhKuenYoung <rppjpp@in-comms.co.kr>
2019-07-30 17:49:16 +0900

    패럴렉스 예제  1차 업로드

---
+++ src/OhKuenYoung/20190731/js/parallax.js
@@ -0,0 +1,30 @@
+document.getElementById('box').style.height = window.innerHeight + 'px';
+
+
+function boxReSize() {
+    document.getElementById('box').style.height = window.innerHeight + 'px';
+}
+
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+// handle event
+window.addEventListener("optimizedResize", function() {
+    boxReSize();
+});
