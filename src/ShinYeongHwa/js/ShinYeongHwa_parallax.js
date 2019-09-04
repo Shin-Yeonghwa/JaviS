173fb251324563bb78af2dfbbdf4a64db2209001
YeongHwa-Shin <itdog77@gmail.com>
2019-09-04 10:09:31 +0900

    test 중

--- src/ShinYeongHwa/js/ShinYeongHwa_parallax.js
+++ src/ShinYeongHwa/js/ShinYeongHwa_parallax.js
@@ -1,6 +1,7 @@
 var log = document.getElementById('log');
-
+var clientHeight = document.body.clientHeight;
+console.log(clientHeight);
 window.onscroll = function scrollPosition(){
-    var scrollY = window.pageYOffset;
+    var scrollY = window.scrollY;
     log.textContent = "Scroll position : " + scrollY;
 }
